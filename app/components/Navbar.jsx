6197088fc7583d8494d62213a27d57b0c29f5700
Shashank <varshneys495@gmail.com>
2025-04-18 20:25:46 +0530

    Updated navbar's background and added tailwind.config.mjs

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,21 +5,25 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
+        <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+            <Image src={assets.header_bg_color} alt='' className='w-full'/>
+        </div>
+
         <nav className='w-full fixed px-5 lg:px-8 xl:px-[-8%] py-4 flex justify-between items-center z-50'>
             <a href='#top'>
                 <Image src={assets.logo} alt='' className='w-28 cursor-pointer mr-14'/>
             </a>
 
-            <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
-                <li><a href='#top'>Home</a></li>
-                <li><a href='#about'>About me</a></li>
-                <li><a href='#services'>Services</a></li>
-                <li><a href='#work'>My Work</a></li>
-                <li><a href='#contact'>Contact me</a></li>
+            <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+                <li><a href='#top' className='font-Ovo'>Home</a></li>
+                <li><a href='#about' className='font-Ovo'>About me</a></li>
+                <li><a href='#services' className='font-Ovo'>Services</a></li>
+                <li><a href='#work' className='font-Ovo'>My Work</a></li>
+                <li><a href='#contact' className='font-Ovo'>Contact me</a></li>
             </ul>
 
             <div>
-                <a href='#contact' className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contact <Image src={assets.arrow_icon} alt='' className='w-3'/></a>
+                <a href='#contact' className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact <Image src={assets.arrow_icon} alt='' className='w-3'/></a>
             </div>
         </nav>
     </>
