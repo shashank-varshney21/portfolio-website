30f56cbaa3d4a8b7c414bb6d4bbe902c234bb521
Shashank <varshneys495@gmail.com>
2025-04-20 13:32:50 +0530

    updated the translate and shoadow property of about list items

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
 
                 <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
                     {infoList.map(({icon, iconDark, title, description}, index)=>(
-                        <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer' key={index}>
+                        <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 hover:shadow-[4px_4px_0_#000]' key={index}>
                             <Image src={icon} alt='title' className='w-7 mt-3'/>
                             <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
                             <p className='text-gray-600 text-sm'>{description}</p>
