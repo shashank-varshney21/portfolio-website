693b98255a43e9d8f06cfd56f8614d25762e3412
Shashank <varshneys495@gmail.com>
2025-04-20 14:03:57 +0530

    Added dynamic css properties using useState and useEffect in nav tag

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Navbar = () => {
 
@@ -12,13 +12,24 @@ const Navbar = () => {
         sideMenuRef.current.style.transform = 'translateX(16rem)'
     }
 
+    const [isScroll, setIsScroll] = useState(false);
+
+    useEffect(()=>{
+        window.addEventListener(scroll,()=> {
+            if(scrollY>50){
+                setIsScroll(true);
+            }else{
+                setIsScroll(false);
+            }
+        })
+    },[])
   return (
     <>
         <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
             <Image src={assets.header_bg_color} alt='' className='w-full'/>
         </div>
 
-        <nav className='w-full fixed px-5 lg:px-8 xl:px-[-8%] py-4 flex justify-between items-center z-50'>
+        <nav className={`w-full fixed px-5 lg:px-8 xl:px-[-8%] py-4 flex justify-between items-center z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : "" }`}>
             <a href='#top'>
                 <Image src={assets.logo} alt='' className='w-28 cursor-pointer mr-14'/>
             </a>
