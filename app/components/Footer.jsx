58f23ba577bbcdaee3d08d2f5a14eeedd700799e
Shashank <varshneys495@gmail.com>
2025-04-21 00:29:08 +0530

    Added dark theme css for the Footer component

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,13 +2,13 @@ import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Footer = () => {
+const Footer = ({isDarkMode}) => {
   return (
     <div className='mt-20'>
         <div className='text-center'>
-            <Image src={assets.logo} alt='' className='w-36 mx-auto'/>
+            <Image src={ isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-36 mx-auto'/>
             <div className='w-max flex items-center gap-2 mx-auto'>
-                <Image src={assets.mail_icon} alt='' className='w-6'/> varshneys495@gmail.com
+                <Image src={ isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/> varshneys495@gmail.com
             </div>
         </div>
 
