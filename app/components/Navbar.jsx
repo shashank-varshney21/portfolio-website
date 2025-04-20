02b1ba9ebd7bcef4c085dfc7f7c7eb9309f4b12d
Shashank <varshneys495@gmail.com>
2025-04-20 23:55:51 +0530

    updated all the dark theme css for the Navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -25,16 +25,16 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
     },[])
   return (
     <>
-        <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+        <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden'>
             <Image src={assets.header_bg_color} alt='' className='w-full'/>
         </div>
 
-        <nav className={`w-full fixed px-5 lg:px-8 xl:px-[-8%] py-4 flex justify-between items-center z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : "" }`}>
+        <nav className={`w-full fixed px-5 lg:px-8 xl:px-[-8%] py-4 flex justify-between items-center z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-[#11001F] dark:shadow-white/20" : "" }`}>
             <a href='#top'>
-                <Image src={assets.logo} alt='' className='w-28 cursor-pointer mr-14'/>
+                <Image src={ isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-28 cursor-pointer mr-14'/>
             </a>
 
-            <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"}`}>
+            <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"}`}>
                 <li><a href='#top' className='font-Ovo'>Home</a></li>
                 <li><a href='#about' className='font-Ovo'>About me</a></li>
                 <li><a href='#services' className='font-Ovo'>Services</a></li>
@@ -44,22 +44,22 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
 
             <div className='flex items-center gap-4'>
                 <button onClick={()=>setIsDarkMode(prev => !prev)}>
-                    <Image src={assets.moon_icon} alt='' className='w-6'/>
+                    <Image src={isDarkMode ? assets.sun_icon :assets.moon_icon} alt='' className='w-6 cursor-pointer'/>
                 </button>
 
-                <a href='#contact' className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact <Image src={assets.arrow_icon} alt='' className='w-3'/></a>
+                <a href='#contact' className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo dark:border-white/50'>Contact <Image src={ isDarkMode ?assets.arrow_icon_dark : assets.arrow_icon} alt='' className='w-3'/></a>
 
                 <button className='block md:hidden ml-3' onClick={openMenu}>
-                    <Image src={assets.menu_black} alt='' className='w-6'/>
+                    <Image src={ isDarkMode ? assets.menu_white : assets.menu_black} alt='' className='w-6'/>
                 </button>
             </div>
             
             {/* ------Mobile Menu--------*/}
 
-            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-[#2a004a] dark:text-white'>
 
                 <div className='absolute right-6 top-6' onClick={closeMenu}>
-                    <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+                    <Image src={ isDarkMode ? assets.close_white : assets.close_black} alt='' className='w-5 cursor-pointer'/>
                 </div>
 
                 <li><a href='#top' onClick={closeMenu} className='font-Ovo'>Home</a></li>
