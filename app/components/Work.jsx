f8f063206eae995d109fb89009ceeebb86c59462
Shashank <varshneys495@gmail.com>
2025-04-20 17:26:57 +0530

    Customised the Work section

---
+++ app/components/Work.jsx
@@ -0,0 +1,34 @@
+import { assets, workData } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Work = () => {
+    return (
+        <div id='Work' className='w-full px-[12%] py-10 scroll-mt-20'>
+            <h4 className='text-center mb-2 text-lg font-Ovo'>My portfolio</h4>
+            <h2 className='text-center text-5xl font-Ovo'>My latest work</h2>
+
+            <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+                I am a frontend developer from India with 3 years of Experience in multiple companies like Microsoft, Tesla and Apple.
+            </p>
+
+            <div className='grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] my-10 gap-5'>
+                {workData.map((project, index) => (
+                    <div key={index} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group' style={{ backgroundImage: `url(${project.bgImage})` }}>
+                        <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
+                            <div>
+                                <h2 className='font-semibold'>{project.title}</h2>
+                                <p className='text-sm text-gray-700'>{project.description}</p>
+                            </div>
+                            <div className='border rounded-full border-black w-9 aspect-square flex items-center justify-between shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition'>
+                                <Image src={assets.send_icon} alt='send_icon' className='w-5' />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Work
