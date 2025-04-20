906b2dafb5e467c44100b21c11939e70b97f9782
Shashank <varshneys495@gmail.com>
2025-04-21 00:26:15 +0530

    Added dark theme css for the Contact component

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
     };
 
         return (
-            <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png bg-no-repeat bg-center bg-[length:90%_auto]")]'>
+            <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png bg-no-repeat bg-center bg-[length:90%_auto]")] dark:bg-none'>
                 <h4 className='text-center mb-2 text-lg font-Ovo'>Connect with me</h4>
                 <h2 className='text-center text-5xl font-Ovo'>Get in touch</h2>
 
@@ -40,11 +40,11 @@ const Contact = () => {
 
                 <form onSubmit={onSubmit} className='max-w-2xl mx-auto'>
                     <div className='grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-6 mt-10 mb-8'>
-                        <input type='text' name='name' placeholder='Enter your name' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' />
-                        <input type='email' name='email' placeholder='Enter your Email' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' />
+                        <input type='text' name='name' placeholder='Enter your name' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-[#2a004a]/30 dark:border-white/90' />
+                        <input type='email' name='email' placeholder='Enter your Email' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-[#2a004a]/30 dark:border-white/90' />
                     </div>
-                    <textarea rows={6} name='message' placeholder='Enter your message' required className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6' />
-                    <button type='submit' className='py3 px-8 w-max flex items-center justify-between gap-2 bg-black text-white rounded-full mx-auto hover:bg-black/80 duration-500'>Submit now <Image src={assets.right_arrow_white} alt='arrow' className='w-4' /></button>
+                    <textarea rows={6} name='message' placeholder='Enter your message' required className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-[#2a004a]/30 dark:border-white/90' />
+                    <button type='submit' className='py3 px-8 w-max flex items-center justify-between gap-2 bg-black text-white rounded-full mx-auto hover:bg-black/80 duration-500 dark:bg-transparent dark:border-[0.5px] dark:hover:bg-[#2a004a]'>Submit now <Image src={assets.right_arrow_white} alt='arrow' className='w-4' /></button>
                     <p className='mt-4'>{result}</p>
                 </form>
             </div>
