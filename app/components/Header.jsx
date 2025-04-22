09beacbbcfd79c9cb6e8a9a2ee55a92834cf5555
Shashank <varshneys495@gmail.com>
2025-04-22 12:49:32 +0530

    Added framer motion in Header component

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,21 +1,46 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from 'motion/react'
 
 const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl text-center h-screen mx-auto flex flex-col items-center justify-center gap-4'>
-        <div>
+        <motion.div 
+          initial={{ scale: 0 }}
+          whileInView={{ scale: 1 }}
+          transition={{ duration: 0.8, type: 'spring', stiffness: 100 }}
+          >
             <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
-        </div>
-        <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>Hi! I'm Shashank Varshney <Image src={assets.hand_icon} alt='' className='w-6'/></h3>
-        <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>frontend developer based in India</h1>
-        <p className='max-w-2xl mx-auto font-Ovo'>
+        </motion.div>
+        <motion.h3 
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.3 }}
+        className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>Hi! I'm Shashank Varshney <Image src={assets.hand_icon} alt='' className='w-6'/></motion.h3>
+        <motion.h1 
+        initial={{ opacity: 0, y: -30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.5 }}
+        className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>frontend developer based in India</motion.h1>
+        <motion.p 
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1}}
+        transition={{ duration: 0.6, delay: 0.7 }}
+        className='max-w-2xl mx-auto font-Ovo'>
             I am a frontend developer from India with 10 years of experience in multiple companies like Microsoft, Tesla and Apple
-        </p>
+        </motion.p>
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-        <a href='#contact' className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
-        <a href='/sample-resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume <Image src={assets.download_icon} alt='' className='w-4'/></a>
+        <motion.a 
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 1 }}
+        href='#contact' className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></motion.a>
+        <motion.a 
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 1 }}
+        href='/sample-resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume <Image src={assets.download_icon} alt='' className='w-4'/></motion.a>
         </div>
     </div>
   )
