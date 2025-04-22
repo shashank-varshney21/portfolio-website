34fc3f402d5de7676ba1d9809c6b3203a751b5ed
Shashank <varshneys495@gmail.com>
2025-04-22 13:25:46 +0530

    Added the framer motion to the Services component

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,30 +1,53 @@
 import { assets, serviceData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from 'motion/react'
 
 const Services = () => {
   return (
-    <div id='services' className='w-full px-[12%] py-10 scroll-mt-20'>
-        <h4 className='text-center mb-2 text-lg font-Ovo'>What I offer</h4>
-        <h2 className='text-center text-5xl font-Ovo'>My services</h2>
+    <motion.div 
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+    id='services' className='w-full px-[12%] py-10 scroll-mt-20'>
+        <motion.h4 
+            initial={{ opacity: 0, y: -20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.3 }}
+        className='text-center mb-2 text-lg font-Ovo'>What I offer</motion.h4>
+        <motion.h2 
+            initial={{ opacity: 0, y: -20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+        className='text-center text-5xl font-Ovo'>My services</motion.h2>
 
-        <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+        <motion.p 
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.7 }}
+        className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
             I am a frontend developer from India with 3 years of Experience in multiple companies like Microsoft, Tesla and Apple.
-        </p>
+        </motion.p>
 
-        <div className='grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-6 my-10'>
+        <motion.div 
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.6, delay: 0.9 }}
+        className='grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-6 my-10'>
             {serviceData.map(({icon,title,description,link},index)=>(
-                <div key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-[4px_4px_0_#000] cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 dark:hover:bg-[#2a004a] dark:hover:shadow-white'> 
+                <motion.div 
+                    whileHover={{ scale: 1.05 }}
+                key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-[4px_4px_0_#000] cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 dark:hover:bg-[#2a004a] dark:hover:shadow-white'> 
                     <Image src={icon} alt='' className='w-10' />
                     <h3 className='text-lg my-4 text-gray-700 dark:text-white'>{title}</h3>
                     <p className='text-sm text-gray-600 leading-5 dark:text-white/80'>
                         {description}
                     </p>
                     <a href={link} className='flex items-center gap-4 text-sm mt-5'>Read more <Image src={assets.right_arrow} alt=''/></a>
-                </div>
+                </motion.div>
             ))}
-        </div>
-    </div>
+        </motion.div>
+    </motion.div>
   )
 }
 
