b7e8872beedf7c5ee9fa30a2340647dee90e8272
Shashank <varshneys495@gmail.com>
2025-07-11 14:29:10 +0530

    Fixing Typos

--- assets/assets.js
+++ assets/assets.js
@@ -121,7 +121,7 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Skills', description: ' Next.js, React.js, Node.js, Express.js, MongoDB, (MERN Stack)' },
     { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Information Technology' },
     { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
 ];
